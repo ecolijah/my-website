428b34da75b7ce99dd827089aba8c749efcfa590
ecolijah <ecolijah@gmail.com>
2023-07-16 17:37:16 -0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecolijah.github.io',
+  base: '/my-website/',
   plugins: [vue()]
 
 })
